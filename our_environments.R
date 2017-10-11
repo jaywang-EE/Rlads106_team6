ba6d29644798d1218d22ef022a3c406629d0d4af
jaywang-EE <b03901111@ntu.edu.tw>
2017-10-11 10:28:54 +0800

    Add files via upload
    Hw_w4

---
+++ our_environments.R
@@ -0,0 +1,8 @@
+list(
+  Jay = "x86_64-w64-mingw32/x64 (64-bit)",
+  Lu = "x86_64-w64-mingw32/x64 (64-bit)",
+  LV = "x86_64-w64-mingw32/x64 (64-bit)",
+  Wu = "x86_64-w64-mingw32/x64 (64-bit)",
+  Michael = "x86_64-w64-mingw32/x64 (64-bit)",
+  Hong = "x86_64-w64-mingw32/x64 (64-bit)"
+)
